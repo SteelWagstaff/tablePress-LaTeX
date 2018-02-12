01dc2d25c04d72f44416498b88050e08d5422b01
Steel Wagstaff <swagstaff@wisc.edu>
2018-02-12 09:51:35 -0600

    Checks to see if WP Quick LaTeX is installed
    Plugin now checks to see if WP Quick LaTeX is installed at network and site levels before adding tablepress cell content filter.

--- tablepress-latex.php
+++ tablepress-latex.php
@@ -4,9 +4,19 @@
  * Plugin URI: none
  * Description: A plugin that allows users to add Quick LaTeX code to TablePress cells
  * Author: Steel Wagstaff
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author URI: https://steelwagstaff.info
  */
 
 //allows Quick LaTeX content to be added to tablepress cells
-add_filter( 'tablepress_cell_content', 'quicklatex_parser' );
+if ( ! function_exists( 'is_plugin_active_for_network' ) ) {
+    require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+}
+ 
+if ( is_plugin_active_for_network( 'plugin-directory/plugin-file.php' ) ) {
+  add_filter( 'tablepress_cell_content', 'quicklatex_parser' );
+}
+
+else if ( is_plugin_active( 'wp-quicklatex/wp-quicklatex.php' ) ) {  
+  add_filter( 'tablepress_cell_content', 'quicklatex_parser' );
+} 
